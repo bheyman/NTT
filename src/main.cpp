7a61587fc2776bcee585d9ea9674e38deceefba0
Benjamin C. Heyman <benjamin.heyman@ieee.org>
2020-09-14 00:43:42 -0400

    seg fault when main function runs, not sure what the issue is

--- src/main.cpp
+++ src/main.cpp
@@ -18,32 +18,18 @@ int main(int argc, char *argv[]){
 	uint64_t p = 17;
 	uint64_t r = 3;	
 	
-	//uint64_t *vec = randVec(n,10);
-	uint64_t vec[8] = {0,1,2,3,4,5,6,7};
+	uint64_t *vec = randVec(n,10);
+	cout << &vec << endl;
 
-	uint64_t *out1;
-	out1 = (uint64_t *)malloc(n*sizeof(uint64_t));	
-	uint64_t *out2;
-	out2 = (uint64_t *)malloc(n*sizeof(uint64_t));
-	uint64_t *out3;
-	out3 = (uint64_t *)malloc(n*sizeof(uint64_t));
-	uint64_t *out4;
-	out4 = (uint64_t *)malloc(n*sizeof(uint64_t));
-	uint64_t *out5;
-	out5 = (uint64_t *)malloc(n*sizeof(uint64_t));
+	uint64_t *out1 = naiveNTT(vec,n,p,r);
+	cout << &out1 << endl;
+	
+	uint64_t *out2 = inPlaceNTT_DIF(vec,n,p,r);
+	cout << &out2 << endl;
 
-	out1 = naiveNTT(vec, n, p, r);		
-	out2 = outOfPlaceNTT_DIT(vec,n,p,r);
-	out3 = outOfPlaceNTT_DIF(vec,n,p,r);
-	out4 = inPlaceNTT_DIT(vec,n,p,r);
-	out5 = inPlaceNTT_DIF(vec,n,p,r);
+	printVec(out1,n);
+	printVec(out2,n);
 
-//	printVec(vec,n);
-	printVec(out1, n);
-	printVec(out2, n);
-	printVec(bit_reverse(out3,n), n);
-	printVec(bit_reverse(out4,n), n);
-	printVec(bit_reverse(out5,n), n);
 
 	return 0;
 
