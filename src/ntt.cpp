c38f13eb1f3444a4e2c03ffb7a2b6f9b910b46fc
Benjamin Corey <23003718+bheyman@users.noreply.github.com>
2021-03-24 21:47:03 -0400

    Update ntt.cpp

--- src/ntt.cpp
+++ src/ntt.cpp
@@ -2,8 +2,6 @@
 #include <cstdint>		/* uint64_t */
 #include <cstdlib> 		/* malloc() */
 
-#include <iostream>
-
 #include "../include/utils.h"	/* bit_reverse(), modExp(), modulo() */
 
 #include "../include/ntt.h" 	//INCLUDE HEADER FILE
@@ -101,7 +99,7 @@ uint64_t *outOfPlaceNTT_DIT(uint64_t *vec, uint64_t n, uint64_t p, uint64_t r){
 
 /**
  * Perform an out-of-place decimation-in-frequency Cooley-Tukey NTT on an input vector and return the result
- * TODO: The output of this method is currently bit-reversed. Hard to fix because it's implemented recursively
+ * NOTE: Output will be bit-reversed
  *
  * @param vec 	The input vector to be transformed
  * @param n	The size of the input vector
@@ -295,44 +293,4 @@ uint64_t *inPlaceNTT_DIT_precomp(uint64_t *vec, uint64_t n, uint64_t p, uint64_t
 
 	return result;
 
-}
-
-uint64_t *inPlaceNTT_DIF_precomp(uint64_t *vec, uint64_t n, uint64_t p, uint64_t r, uint64_t *twiddle, bool rev){
-
-	/* TODO: I don't think this works yet */
-
-	uint64_t *result;
-	result = (uint64_t *) malloc(n*sizeof(uint64_t));
-
-	for(uint64_t i = 0; i < n; i++){
-		result[i] = vec[i];
-	}
-
-	uint64_t m,k_,a,factor1,factor2;
-	for(uint64_t i = log2(n); i >= 1; i--){
-
-		m = pow(2,i);
-
-		k_ = (p - 1)/m;
-		a = modExp(r,k_,p);
-
-		for(uint64_t j = 0; j < n; j+=m){
-
-			for(uint64_t k = 0; k < m/2; k++){
-
-				factor1 = result[j + k];
-				factor2 = result[j + k + m/2];
-
-				result[j + k] 		= modulo(factor1 + factor2,p);
-				result[j + k + m/2]	= modulo(modExp(a,k,p)*modulo(factor1 - factor2,p),p);
-
-			}
-		}
-	}
-	
-	if(rev){
-		return bit_reverse(result,n);
-	}else{
-		return result;
-	}
 }
